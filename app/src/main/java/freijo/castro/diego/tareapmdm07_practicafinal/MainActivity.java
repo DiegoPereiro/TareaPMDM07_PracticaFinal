58ebfe8a3718ebcfdb5cc0771d4446b7f15d0434
DiegoPereiro <diegocfpereiro@gmail.com>
2019-05-06 23:01:55 +0200

    Atctivity de presentacion

--- app/src/main/java/freijo/castro/diego/tareapmdm07_practicafinal/MainActivity.java
+++ app/src/main/java/freijo/castro/diego/tareapmdm07_practicafinal/MainActivity.java
@@ -1,13 +1,26 @@
 package freijo.castro.diego.tareapmdm07_practicafinal;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText etUsuario, etcontraseña;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etUsuario=(EditText) findViewById(R.id.etUsuario);
+        etcontraseña=(EditText) findViewById(R.id.etContraseña);
+    }
+
+    public void iniciarSesion(View view) {
+
+
     }
 }
