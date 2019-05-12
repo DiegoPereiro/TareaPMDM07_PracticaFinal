e7b388f7edf4d41b15faccc3fa2a706777a3639b
DiegoPereiro <diegocfpereiro@gmail.com>
2019-05-12 22:51:43 +0200

    crear base de datos y credenciales de usuario

--- app/src/main/java/freijo/castro/diego/tareapmdm07_practicafinal/MainActivity.java
+++ app/src/main/java/freijo/castro/diego/tareapmdm07_practicafinal/MainActivity.java
@@ -1,12 +1,21 @@
 package freijo.castro.diego.tareapmdm07_practicafinal;
 
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import freijo.castro.diego.tareapmdm07_practicafinal.basedatos.BaseDatos;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int version = 1;
+    private SQLiteDatabase baseDatos;
+
     private EditText etUsuario, etcontraseña;
 
 
@@ -15,13 +24,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        etUsuario=(EditText) findViewById(R.id.etUsuario);
-        etcontraseña=(EditText) findViewById(R.id.etContraseña);
+        etUsuario = (EditText) findViewById(R.id.etUsuario);
+        etcontraseña = (EditText) findViewById(R.id.etContraseña);
+
+
+        BaseDatos dbatos = new BaseDatos(MainActivity.this, "bdPmdm", null, version);
+        baseDatos = dbatos.getWritableDatabase();
+
+        Cursor usuarios = baseDatos.rawQuery("SELECT * FROM usuarios", null);
+        if (!usuarios.moveToFirst()) {
+            baseDatos.execSQL("INSERT INTO usuarios (usuario, contraseña) VALUES ('DIEGO', '1234')");
+        }
+
     }
 
     public void iniciarSesion(View view) {
-        Intent intent=new Intent(this, Principal.class);
-        startActivity(intent);
-        finish();
+        String usuario=etUsuario.getText().toString();
+        String constraseña=etcontraseña.getText().toString();
+
+
+        if (!usuario.equals("") && !constraseña.equals("")) {
+            Cursor csUsuario=baseDatos.rawQuery("select * from usuarios where usuario='"+usuario+"' and contraseña='"+constraseña+"'", null);
+            if (csUsuario.moveToFirst()){
+                Intent intent = new Intent(this, Principal.class);
+                startActivity(intent);
+                finish();
+            }else {
+                Toast.makeText(this, "Credenciales incorrectas", Toast.LENGTH_SHORT).show();
+            }
+        }else {
+            Toast.makeText(this, "Debe poner el usuario y la contraseña", Toast.LENGTH_SHORT).show();
+        }
+
+
+
     }
 }
---
+++ app/src/main/java/freijo/castro/diego/tareapmdm07_practicafinal/basedatos/BaseDatos.java
@@ -0,0 +1,50 @@
+package freijo.castro.diego.tareapmdm07_practicafinal.basedatos;
+
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+public class BaseDatos extends SQLiteOpenHelper {
+
+    private String tbusuarios = "CREATE TABLE usuarios (id INTEGER PRIMARY KEY AUTOINCREMENT, usuario TEXT, contraseña TEXT)";
+    private String tbClientes = "CREATE TABLE clientes (id INTEGER PRIMARY KEY AUTOINCREMENT, cif TEXT, nombre TEXT, via TEXT, direccion TEXT, numero TEXT, escalera TEXT, piso TEXT, puerta TEXT, codigopostal TEXT, poblacion TEXT, provincia TEXT, pais TEXT, telefono TEXT, telefono2 TEXT, fax TEXT, email TEXT, iban TEXT, numpagos INTEGER DEFAULT 0, pago1 TEXT, pago2 TEXT, pago3 TEXT, pago4 TEXT, formapago TEXT, reparto TEXT, imprimircuentaempresa INTEGER DEFAULT 0, recargo INTEGER DEFAULT 0,  incrementonini REAL, incrementonormal REAL)";
+
+
+    public BaseDatos(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(tbusuarios);
+        db.execSQL(tbClientes);
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        String tabla;
+        tabla=tbusuarios;
+        try {db.execSQL(tabla);} catch (SQLiteException e) {actualizarCampos(tabla, db);}
+        tabla = tbClientes;
+        try {db.execSQL(tabla);} catch (SQLiteException e) {actualizarCampos(tabla, db);}
+
+    }
+
+    private void actualizarCampos(String tabla, SQLiteDatabase db) {
+        String nombreTabla = tabla.substring(12, tabla.indexOf("("));
+        String[] campos = tabla.substring(tabla.indexOf("(") + 1, tabla.indexOf(")")).split(",");
+
+        for (int i = 0; i < campos.length; i++) {
+            try {
+                db.execSQL("alter table " + nombreTabla + " add " + campos[i].trim());
+            } catch (SQLiteException e) {
+            }
+        }
+    }
+}
